df47e0b948178c73e319016560cf61db8f829be0
Simon Cass <scass1991@gmail.com>
2019-03-06 16:32:06 +0000

    Cell state tests added - failing

---
+++ spec/cellStateSpec.js
@@ -0,0 +1,7 @@
+describe("Cell State", () => {
+
+  it("Has a state of 1 if ALIVE", () => {
+    expect(cellState.ALIVE).toEqual(1)
+  })
+
+})
--- src/game.js
+++ src/game.js
@@ -14,13 +14,3 @@ function cellCount(arr) {
 function arrSum(arr){
   return arr.reduce(function(a,b){return a + b}, 0);
 }
-
-// function arrayFiller(arr) {
-//   for (let i=0; i < arrayCreation.x; i++) {
-//     for (let j=0; j < arrayCreation.y; j++) {
-//       arr.fill(floor(Math.random(2)), 1);
-//     }
-//   }
-// }
-//
-// arr.fill().map( () => Math.floor(Math.random(2)))
